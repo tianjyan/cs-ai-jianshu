5dbe509bdc7036a4cd8275807204e2c6988772f9
youngytj <youngytj@sina.com>
2015-12-03 13:45:59 +0800

    Add Init WebChat

---
+++ SocialShare/WeChat/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialShare.WeChat
+{
+    internal class Config
+    {
+        internal const string Key = "Put Your Key";
+        internal const string Secret = "Put Your Secret";
+    }
+}
---
+++ SocialShare/WeChat/WechatClient.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SocialShare.WeChat
+{
+    public class WechatClient
+    {
+        
+    }
+}
